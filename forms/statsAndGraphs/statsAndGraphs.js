8d20d89f507a395f9b71562ff67bb657b0a853c5
Anthony Laurenzo <anthonylaurenzo@Anthonys-MacBook-Air-2.local>
2019-11-21 21:20:13 -0600

    A big update for  Matt!

--- forms/statsAndGraphs/statsAndGraphs.js
+++ forms/statsAndGraphs/statsAndGraphs.js
@@ -1,88 +1,179 @@
-let teamPlayedAgainst = []
-let teamPlayedAs = []
-let iptFgPercentage = []
-let iptAssists = []
-let iptsRebounds = []
-let iptYourScore = []
-let iptOpponentScore = []
-let iptTurnoverMargin = []
-let iptTOP = []
-
-  
 statsAndGraphs.onshow=function(){
   teamPlayedAs.clear()
-  let query = "SELECT names FROM teams"
-  req1 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=ajl98875&pass=Stlouiesunsv2@&database=ajl98875&query=" + query)
+  let query = "SELECT team_name FROM teams"
+  req1 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=ajl98875&pass=Stlouiesunsv2@&database=375groupb6&query=" + query)
   
-  if (req1.status == 200) { 
+  if (req1.status == 200) { //transit worked.
     let results = JSON.parse(req1.responseText)
     
     //get the array
     if (results.length == 0)
-        txtDetails.value("Error, no teams found.")
+        txtDetail.value("There are no teams.")
     else {        
+  
         let message = ""
         for (i = 0; i <= results.length - 1; i++){
             message = message + results[i][0] + "\n"
             teamPlayedAs.addItem(results[i][0])
         }
-        }
      } 
-   
-  teamPlayedAgainst.clear()
-  let query1 = "SELECT names FROM teams"
-  req3 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=ajl98875&pass=Stlouiesunsv2@&database=ajl98875&query=" + query)
-  
-  if (req3.status == 200) { 
-    let results = JSON.parse(req1.responseText)
-    
-    //get the array
-    if (results.length == 0)
-        txtDetails.value("Error, no teams found.")
-    else {        
-        let message = ""
-        for (i = 0; i <= results.length - 1; i++){
-            message = message + results[i][0] + "\n"
-            teamPlayedAgainst.addItem(results[i][0])
-        }
-     } 
-  } 
+  } else{
+      NSB.MsgBox("Something run")
   }
-  
+
+}
 
 teamPlayedAs.onclick=function(s){
-    if (typeof(s) == "object"){  
-    return                    
+    if (typeof(s) == "object"){  // means control clicked but no selection made yet
+    return                     // do nothing
     }else {
-      teamPlayedAs.value = s   
+      teamPlayedAs.value = s   // make dropdown show choice user made
+      
+      //get the information from the database
+      let query2 = "SELECT team_name FROM teams WHERE team_name=" + '"' + teamPlayedAs.selection + '"' 
+      req2 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=ajl98875&pass=Stlouiesunsv2@&database=375groupb6&query=" + query2)
       
       
-     // let query2 = "INSERT INTO statistics (team_name) VALUES (teamPlayedAs.selection) + '"' 
-      req2 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=ajl98875&pass=Stlouiesunsv2@&database=ajl98875&query=" + query2)
-      
-      
-      if (req2.status == 200) { 
+      if (req2.status == 200) { //transit worked.
           let results2 = JSON.parse(req2.responseText)
         
           //get the array
           if (results2.length == 0){
-              txtDetails.value("No team.")
+              txtDetail.value("There are no teams.")
           }else {        
-              
+              console.log("the parsed JSON is " + results2)
+              console.log("eg. temp[0] or first row in big array is " + results2[0])
+              console.log("to get to Paul, need results[0][1]: " + results2[0][2])
               let message2 = ""
               for (i = 1; i <= 2; i++)
                   message2 = message2 + results2[0][i] + "\n"
               for (i =3; i <=5; i++)
                   message2 = message2 + results2[0][i] + ", "
-              txtDetails.value = message2
+              txtDetail.value = message2
           } 
+      }else{
+        NSB.MsgBox("Something wrong")
+      }
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+  teamPlayedAgainst.clear()
+  let query3 = "SELECT team_name FROM teams"
+  req3 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=ajl98875&pass=Stlouiesunsv2@&database=375groupb6&query=" + query3)
+  
+  if (req3.status == 200) { //transit worked.
+    let results3 = JSON.parse(req3.responseText)
+    
+    //get the array
+    if (results3.length == 0)
+        txtDetail.value("There are no teams.")
+    else {        
+  
+        let message3 = ""
+        for (i = 0; i <= results.length - 1; i++){
+            message3 = message3 + results3[i][0] + "\n"
+            teamPlayedAgainst.addItem(results3[i][0])
+        }
+     } 
+  } else{
+      NSB.MsgBox("Something run")
+  }
+
+}
+
+
+teamPlayedAgainst.onclick=function(s){
+    if (typeof(s) == "object"){  // means control clicked but no selection made yet
+    return                     // do nothing
+    }else {
+      teamPlayedAgainst.value = s   // make dropdown show choice user made
+      
+      //get the information from the database
+      let query4 = "SELECT team_name FROM teams WHERE team_name=" + '"' + teamPlayedAgainst.selection + '"' 
+      req4 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=ajl98875&pass=Stlouiesunsv2@&database=375groupb6&query=" + query4)
+      
+      
+      if (req4.status == 200) { //transit worked.
+          let results4 = JSON.parse(req4.responseText)
+        
+          //get the array
+          if (results4.length == 0){
+              txtDetail.value("There are no company of that type.")
+          }else {        
+              console.log("the parsed JSON is " + results4)
+              console.log("eg. temp[0] or first row in big array is " + results4[0])
+              console.log("to get to Paul, need results[0][1]: " + results4[0][2])
+              let message4 = ""
+              for (i = 1; i <= 2; i++)
+                  message4 = message4 + results4[0][i] + "\n"
+              for (i =3; i <=5; i++)
+                  message4 = message4 + results4[0][i] + ", "
+              txtDetail.value = message4
+          } 
+      }else{
+        NSB.MsgBox("Something wrong")
       }
    }
 }
 
+*/
+
+let teamYouUsed = []
+let teamYouFaced = []
+let fgPercentage = []
+let assists = []
+let rebounds = []
+let yourScore = []
+let opponentScore = []
+let turnoverMargin = []
+let tOP = []
+
+
+btnSubmit.onclick=function(){
+let teamYouUsed = 
+let teamYouFaced = 
+let iptFgPercentage = 
+let iptAssists = 
+let iptsRebounds = 
+let iptYourScore = 
+let iptOpponentScore = 
+let iptTurnoverMargin = 
+let iptTOP = 
+  let queryInsert = "INSERT INTO customer (name,street,city,state,zipcode) VALUES ('"+newNameA+"', '"+newStreetA+"', '"+newCityA+"','"+ newStateA+"'," +newZipCodeA+")"
+
+    req2 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=sch54620&pass=C19971106w&database=sch54620&query=" + queryInsert)
+
+    if (req2.status == 200) { //transit worked.
+        if (req2.responseText == 500) {   // means the insert succeeded
 
 
 
 
 
 
+
+
+  
--- forms/uploadVideo/uploadVideo.js
+++ forms/uploadVideo/uploadVideo.js
@@ -1,42 +1,83 @@
-reader = new FileReader();
-var fileData;
+/* need to
+1. update AJAX database login to your login
+2. update queries to match your tables and fields
+*/
 
-txtVideo.onchange = function() {
-  fileType = Mid(txtVideo.value, InStrRev(txtVideo.value,"."));
-  switch (true) {
-  case ((fileType) == ".avi" ): case ((fileType) ==  ".mp4" ): case ((fileType) ==  ".mov"):
-    reader.readAsDataURL(txtVideo.files[0]);
-	break;
-  case ((fileType) == ".png" ): case ((fileType) ==  ".gif" ): case ((fileType) ==  ".jpg"):
-    reader.readAsDataURL(txtVideo.files[0]);
-	break;
-  default:
-    reader.readAsBinaryString(txtVideo.files[0]);
-  }
+
+/*
+
+let let picBase64 = ""
+let pic1 = ""
+let userData = ''
+
+reader = new FileReader()
+
+txtGetPicture.onchange = function() {
+// upload picture from filesystem on laptop or photos on phone (deploy to Volt for phone)
+     //This gets called after the picture is chosen. 
+     //Next, let's read start reading it in.
+     //The _onload function will be called when that completes.
+     reader.readAsDataURL(txtGetPicture.files[0])
 };
 
-reader.onload = function() {
-  fileData = reader.result;
+reader.onload = function(e) {
+     //read of the file is complete.
+     //Now, let's load it into an image.
+     //The _onload function for the image will be called on completion.
+  Image1.firstChild.src = e.target.result //Phone Viewing ONLY
+  picBase64 = e.target.result
+  return
 };
+  
+save.onclick=function(){ 
+// saves picture into database
+    let netID2 = "ajl98875"
+    let fName2 = "anthony"
+    //alert(standing.selectedItem())
+    // let picture = txtGetPicture.files[0]
+        
+    let query = "INSERT INTO user (netID,name,photo) VALUES ('"+netID2+"','"+fName2+"','"+picBase64+"')" 
+    let req3 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=ajl98875&pass=Stlouiesunsv2@&database=ajl98875&query=" + query)
 
-btnUpload.onclick = function() {
-  if(Trim(txtVideo.value) != "") {
-    fileName = encodeURIComponent(Replace(txtPhoto.value, "C:\\fakepath\\" , ""));
-    fileContent = encodeURIComponent(fileData);
- } else {
-    fileName = encodeURIComponent(txtFileName.value);
-    fileContent = encodeURIComponent(txaContents.value);
-  }
-};
-
-function done() {
-  if(req.status == 200) { //success
-    NSB.MsgBox("File uploaded!");
-    txtVideo.value = "";
- } else { //failure
-    msg = "Error: Status = "  +  req.status;
-    if(TypeName(req.statusText)=="string" ) { msg = msg  +  " "  +  req.statusText; }
-    if(TypeName(req.err)=="string" ) { msg = msg  +  " "  +  req.error; }
-    NSB.MsgBox(msg);
-  }
+    if (req3.status == 200) { //everything worked. :)
+        alert("User added"); 
+        NSB.MsgBox("Thank you for joining us, "+fName2 +"!", 0, "Welcome!");
+        ChangeForm(studentSearch, "fade");
+    } else {
+        alert("Error: " + req3.status)
+    }  
 }
+
+
+btnInsertPic.onclick=function(){
+// inserts picture from database into the control
+    myQuery = "SELECT * FROM user WHERE netID = 'clc90595'"
+    req2 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=ajl98875&pass=Stlouiesunsv2@&database=ajl98875&query=" + myQuery);
+    if  (req2.status == 200) { //everything worked. 
+        //2D array, with [row][colum]
+        alert(req2.responseText)
+        
+        userData = JSON.parse(req2.responseText);  
+        
+        //             0      1     2    
+        //Columns go  netID name photo       
+        cindyUser = userData[0][0] + ", " + userData[0][1] + "\n"
+        console.log(cindyUser)
+        pic1 = userData[0][2] + '.'
+        
+        pic1 = pic1.replace(/\s/gi, '+') // To replace all spaces with + (multiple spaces will be treated as multiple characters)
+        alert(pic1)
+        
+        pic1Array = Split(pic1, ".")
+        
+        //put the Base64 image into the Image control
+        //Image1.innerHTML = "<img src='" + pic1 + "'/>"
+        Image2.firstChild.src = pic1Array[0]
+    
+      } else {
+        //Handle that. 
+        alert("Error: " + req1.status)
+    }
+}
+
+*/
