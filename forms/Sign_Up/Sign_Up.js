64d3fc26b70397d4bded54284d8ceb17cb7e615e
matthew canfield <matthewcanfield@creighton.edu>
2019-11-24 18:45:41 -0600

    Sign Up Completed

--- forms/Sign_Up/Sign_Up.js
+++ forms/Sign_Up/Sign_Up.js
@@ -8,7 +8,8 @@ let phoneNumber = inptPhoneNumber.value
 let username = inptUsername.value 
 let password = inptPassword.value
 
-let query = `INSERT into users (user_email, username, user_password, first_name, last_name, phone_number) VALUES("'" ${email}"'", "'"${username}"'", "'"${password}"'", "'"${firstName}, "'"${lastName}"'", "'"${phoneNumber}"'");`
+let query = "INSERT INTO users (user_email, username, user_password, first_name, last_name, phone_number) VALUES ('" + email + "', '" + username + "', '" + password + "' , '" + firstName + "' , '" + lastName + "' , '"  + phoneNumber + "')"
+//"INSERT INTO pets (petName,petType) VALUES ('" + name + "', '" + animalType + "')" example from class
    req1 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=mlc55572&pass=cHarliE125!&database=375groupb6&query=" + query)
    
      if (req1.status == 200) {
@@ -16,12 +17,12 @@ let query = `INSERT into users (user_email, username, user_password, first_name,
     
  }   
    //Giver error if there are no results otherwise give a welcome message. 
-    if (results.length == 0)
+    if (req1.responseText.length == 0)
         lblError.value = "Error"
     else {        
         lblError.value = "Success! Welcome to 2King!"
+        ChangeForm(Sign_in)
 }
-console.log(results)
 }
         
 
